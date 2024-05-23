7191ba8df5c43ccd7fba1bcef5b12abf3fc46d75
kimjeonghoon <90254195+kjh9503@users.noreply.github.com>
2024-05-23 16:14:23 +0900

    Update betae_caqr.sh

--- CaQR/betae/betae_caqr.sh
+++ CaQR/betae/betae_caqr.sh
@@ -1,12 +1,12 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_test \
---data_path data/FB15k-237-betae -n 128 -b 512 -d 400 -g 60 \
+--data_path ../data/FB15k-237-betae -n 128 -b 512 -d 400 -g 60 \
 -lr 0.0001 --max_steps 450001 --cpu_num 5 --geo beta --valid_steps 60000 \
 -betam "(1600,2)" --print_on_screen --note betae_caqr \
 --use_hyperrel --PE --RE --SE --table_normalization -sd 400 \
 --fusion_all
 
 CUDA_VISIBLE_DEVICES=1 python main.py --cuda --do_train --do_test \
---data_path data/NELL-betae -n 128 -b 512 -d 400 -g 60 \
+--data_path ../data/NELL-betae -n 128 -b 512 -d 400 -g 60 \
 -lr 0.0001 --max_steps 500000 --cpu_num 5 --geo beta --valid_steps 60000 \
 -betam "(1600,2)" --print_on_screen --note betae_caqr \
 --use_hyperrel --PE --RE --SE --table_normalization -sd 108 \
