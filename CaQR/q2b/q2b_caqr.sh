b6fb320d7d73fc665bef31419813b01ef3c034be
kimjeonghoon <90254195+kjh9503@users.noreply.github.com>
2024-07-24 21:00:34 +0900

    Update q2b_caqr.sh

--- CaQR/q2b/q2b_caqr.sh
+++ CaQR/q2b/q2b_caqr.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python run.py --cuda --do_train --do_test --do_valid \
-  --data_path ../data/FB15k-237-2 --model BoxTransE -n 128 -b 512 -d 400 -g 24 -a 1.0 \
+  --data_path ../data/FB15k-237-betae --model BoxTransE -n 128 -b 512 -d 400 -g 24 -a 1.0 \
   -lr 0.0001 --max_steps 300001 --cpu_num 5 --test_batch_size 16 --center_reg 0.2 \
   --geo box --stepsforpath 300001  --offset_deepsets inductive \
   --center_deepsets eleattention --print_on_screen --note q2b_caqr -sd 108 \
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=1 python run.py --cuda --do_train --do_test --do_valid \
 
 
 CUDA_VISIBLE_DEVICES=0 python run.py --cuda --do_train --do_test --do_valid \
-  --data_path ../data/NELL-2 --model BoxTransE -n 128 -b 512 -d 400 -g 24 -a 1.0 \
+  --data_path ../data/NELL-betae --model BoxTransE -n 128 -b 512 -d 400 -g 24 -a 1.0 \
   -lr 0.0001 --max_steps 300001 --cpu_num 5 --test_batch_size 16 --center_reg 0.2 \
   --geo box --stepsforpath 300001  --offset_deepsets inductive \
   --center_deepsets eleattention --print_on_screen --note q2b_caqr -sd 108 \
